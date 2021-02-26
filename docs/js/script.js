28d72288cac3dd5e5bd95a0da30695bd1bdfc454
mstanka <mice78.mm@gmail.com>
2021-02-26 20:54:50 +0100

    add slider section

--- docs/js/script.js
+++ docs/js/script.js
@@ -1,3 +1,5 @@
+///// Hamburger menu
+
 const nav = document.querySelector(".nav");
 const menu = document.querySelector(".nav__menu");
 const menuIcon = document.querySelector(".nav__menu--icon");
@@ -15,4 +17,39 @@ document.addEventListener("keydown", (e) => {
     nav.classList.remove("nav__menu--active");
     menuIcon.setAttribute("src", "./assets/mobile/icon-hamburger.svg");
   }
+});
+
+///// Slider
+
+const slides = document.querySelectorAll(".slide");
+const btnLeft = document.querySelector(".slider__btn--left");
+const btnRight = document.querySelector(".slider__btn--right");
+let curSlide = 0;
+const maxSlide = slides.length - 1;
+
+const goToSlide = (slide) => {
+  slides.forEach((s, i) => {
+    s.style.transform = `translateX(${100 * (i - slide)}%)`;
+    // curSlide = 0; output: 0, 100%, 200%
+  });
+};
+
+const nextSlide = () => {
+  if (curSlide === maxSlide) curSlide = 0;
+  else curSlide++;
+  goToSlide(curSlide);
+};
+
+const prevSlide = () => {
+  if (curSlide === 0) curSlide = maxSlide;
+  else curSlide--;
+  goToSlide(curSlide);
+};
+
+btnRight.addEventListener("click", nextSlide);
+btnLeft.addEventListener("click", prevSlide);
+
+document.addEventListener("keydown", (e) => {
+  if (e.key === "ArrowLeft") prevSlide();
+  if (e.key === "ArrowRight") nextSlide();
 });
